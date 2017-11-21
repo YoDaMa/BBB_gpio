664edb3fd87a2aa85b35289e366ccb2421a7f501
Yoseph <yoseph.maguire@gmail.com>
2017-11-21 03:43:37 -0600

    asdf

--- src/gpio-lkm.c
+++ src/gpio-lkm.c
@@ -250,7 +250,7 @@ static void custom_set_gpio_dataout_reg(unsigned offset, int enable)
 	u32 l;
 
 	l = readl_relaxed(reg);
-    printk(KERN_INFO "CUSTOM_DATAOUT_REG: Readl_relaxed is %lu", l);
+    // printk(KERN_INFO "CUSTOM_DATAOUT_REG: Readl_relaxed is %lu", l);
 
 	if (enable)
 		l |= gpio_bit;
@@ -268,7 +268,7 @@ static int custom_get_gpio_datain(int offset)
 static int custom_get_gpio_dataout(int offset)
 {
 	void __iomem *reg = (void *) (long) gpio_map + GPIO_DATAOUT;
-    printk(KERN_INFO "CUSTOM_DATAOUT: The register is %p", reg);
+    printk(KERN_INFO "CUSTOM_DATAOUT: The register is ");
 	return (readl_relaxed(reg) & (BIT(offset))) != 0;
 }
 
