06b80ec86f617baf68e6cf3c6f31b9316c9926c5
Yoseph <yoseph.maguire@gmail.com>
2017-11-23 15:24:16 -0600

    stored part 2 and 3

---
+++ badbadbutworking/twoandthree/gpio_simple.c
@@ -0,0 +1,203 @@
+/**
+ * @file   gpio_test.c
+ * @author Derek Molloy
+ * @date   19 April 2015
+ * @brief  A kernel module for controlling a GPIO LED/button pair. The device mounts devices via
+ * sysfs /sys/class/gpio/gpio115 and gpio49. Therefore, this test LKM circuit assumes that an LED
+ * is attached to GPIO 49 which is on P9_23 and the button is attached to GPIO 115 on P9_27. There
+ * is no requirement for a custom overlay, as the pins are in their default mux mode states.
+ * @see http://www.derekmolloy.ie/
+*/
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/gpio.h>                 // Required for the GPIO functions
+#include <linux/interrupt.h>            // Required for the IRQ code
+#include <linux/device.h>         // Header to support the kernel Driver Model
+#include <linux/fs.h>             // Header for the Linux file system support
+#include <asm/uaccess.h>          // Required for the copy to user function
+#include <linux/time.h>
+#include "beaglebone-gpio.h"
+
+#define  DEVICE_NAME "simplegpio424"    ///< The device will appear at /dev/gpio424 using this value
+#define  CLASS_NAME  "elec"        ///< The device class -- this is a character device driver
+
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Derek Molloy");
+MODULE_DESCRIPTION("A Button/LED test driver for the BBB");
+MODULE_VERSION("0.1");
+
+static unsigned int gpioButton = 115;   ///< hard coding the button gpio for this example to P9_27 (GPIO115)
+static unsigned int irqNumber;          ///< Used to share the IRQ number within this file
+static long capacitance;
+static struct timespec tic, toc, timediff;
+static int    majorNumber;                  ///< Stores the device number -- determined automatically
+static struct class*  ebbcharClass  = NULL; ///< The device-driver class struct pointer
+static struct device* ebbcharDevice = NULL; ///< The device-driver device struct pointer
+
+/// Function prototype for the custom IRQ handler function -- see below for the implementation
+static irq_handler_t  ebbgpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs);
+static int     dev_open(struct inode *, struct file *);
+static int     dev_release(struct inode *, struct file *);
+static long dev_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+
+static struct file_operations fops = {
+    .open = dev_open,
+    .release = dev_release,
+    .unlocked_ioctl = dev_ioctl
+};
+
+
+static int dev_open(struct inode *inodep, struct file *filep){
+   printk(KERN_INFO "GPIO_LKM: Device has been opened\n");
+   return 0;
+}
+
+static int dev_release(struct inode *inodep, struct file *filep){
+   printk(KERN_INFO "GPIO_LKM: Device successfully closed\n");
+   return 0;
+}
+
+static long dev_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+    switch (cmd)
+    {
+        case IOCTL_GET_VALUE:
+        {
+            // printk(KERN_INFO "GPIO_LKM: Hello from IOCTL_GET_VALUE\n");
+            capacitance = timediff.tv_nsec;
+            // printk(KERN_INFO "GPIO_LKM: returning capacitance value.\n");
+            return timediff.tv_nsec;
+        }
+        break;
+
+        case IOCTL_MEASURE_CAPACITANCE:
+        {
+            // printk(KERN_INFO "GPIO_LKM: Hello from IOCTL_MEASURE_CAPACITANCE\n");
+            // set direction to out and value to HIGH
+            gpio_direction_output(gpioButton, 1); // set pin to output
+            // NEED TO ADJUST GPIOD_DIRECTION_OUTPUT_RAW IN GPIOLIB.C TO ALLOW OUTPUT
+            // printk(KERN_INFO "GPIO_LKM: GPIO OUT, set to: %d \n",gpio_get_value(gpioButton));
+            getrawmonotonic(&tic);
+            // printk(KERN_INFO "GPIO_LKM: tic (%ld) \n", tic.tv_nsec);
+            gpio_direction_input(gpioButton);
+        }
+        break;
+    }
+    return 0;
+}
+
+
+/** @brief The GPIO IRQ Handler function
+ *  This function is a custom interrupt handler that is attached to the GPIO above. The same interrupt
+ *  handler cannot be invoked concurrently as the interrupt line is masked out until the function is complete.
+ *  This function is static as it should not be invoked directly from outside of this file.
+ *  @param irq    the IRQ number that is associated with the GPIO -- useful for logging.
+ *  @param dev_id the *dev_id that is provided -- can be used to identify which device caused the interrupt
+ *  Not used in this example as NULL is passed.
+ *  @param regs   h/w specific register values -- only really ever used for debugging.
+ *  return returns IRQ_HANDLED if successful -- should return IRQ_NONE otherwise.
+ */
+static irq_handler_t ebbgpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs){
+    getrawmonotonic(&toc);
+    // printk(KERN_INFO "GPIO_LKM: toc (%ld) \n", toc.tv_nsec);
+    timediff = timespec_sub(toc, tic);
+    // printk(KERN_INFO "GPIO_TEST: timediff (%ld) \n", timediff.tv_nsec);
+    gpio_direction_output(gpioButton, 1);
+    // printk(KERN_INFO "GPIO_TEST: GPIO_OUT, set to: %d \n", gpio_get_value(gpioButton));
+    return (irq_handler_t) IRQ_HANDLED;      // Announce that the IRQ has been handled correctly
+}
+
+
+
+/** @brief The LKM initialization function
+ *  The static keyword restricts the visibility of the function to within this C file. The __init
+ *  macro means that for a built-in driver (not a LKM) the function is only used at initialization
+ *  time and that it can be discarded and its memory freed up after that point. In this example this
+ *  function sets up the GPIOs and the IRQ
+ *  @return returns 0 if successful
+ */
+static int __init ebbgpio_init(void){
+   int result = 0;
+   printk(KERN_INFO "GPIO_TEST: Initializing the GPIO_TEST LKM\n");
+
+   // Going to set up the LED. It is a GPIO in output mode and will be on by default
+   gpio_request(gpioButton, "sysfs");       // Set up the gpioButton
+   gpio_direction_input(gpioButton);        // Set the button GPIO to be an input
+   gpio_set_debounce(gpioButton, 200);      // Debounce the button with a delay of 200ms
+   gpio_export(gpioButton, false);          // Causes gpio115 to appear in /sys/class/gpio
+			                    // the bool argument prevents the direction from being changed
+   // Perform a quick test to see that the button is working as expected on LKM load
+   printk(KERN_INFO "GPIO_TEST: The button state is currently: %d\n", gpio_get_value(gpioButton));
+
+   // GPIO numbers and IRQ numbers are not the same! This function performs the mapping for us
+   irqNumber = gpio_to_irq(gpioButton);
+   printk(KERN_INFO "GPIO_TEST: The button is mapped to IRQ: %d\n", irqNumber);
+
+   // This next call requests an interrupt line
+   result = request_irq(irqNumber,             // The interrupt number requested
+                        (irq_handler_t) ebbgpio_irq_handler, // The pointer to the handler function below
+                        IRQF_TRIGGER_LOW,   // Interrupt on rising edge (button press, not release)
+                        "ebb_gpio_handler",    // Used in /proc/interrupts to identify the owner
+                        NULL);                 // The *dev_id for shared interrupt lines, NULL is okay
+
+   printk(KERN_INFO "GPIO_TEST: The interrupt request result is: %d\n", result);
+   
+   
+    // Try to dynamically allocate a major number for the device -- more difficult but worth it
+    majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
+    if (majorNumber<0){
+        printk(KERN_ALERT "GPIO_LKM: failed to register a major number\n");
+        return majorNumber;
+    }
+    printk(KERN_INFO "GPIO_LKM: registered correctly with major number %d\n", majorNumber);
+
+    // Register the device class
+    ebbcharClass = class_create(THIS_MODULE, CLASS_NAME);
+    if (IS_ERR(ebbcharClass)){                // Check for error and clean up if there is
+        unregister_chrdev(majorNumber, DEVICE_NAME);
+        printk(KERN_ALERT "Failed to register device class\n");
+        return PTR_ERR(ebbcharClass);          // Correct way to return an error on a pointer
+    }
+    printk(KERN_INFO "GPIO_LKM: device class registered correctly\n");
+
+    // Register the device driver
+    ebbcharDevice = device_create(ebbcharClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+    if (IS_ERR(ebbcharDevice)){               // Clean up if there is an error
+        class_destroy(ebbcharClass);           // Repeated code but the alternative is goto statements
+        unregister_chrdev(majorNumber, DEVICE_NAME);
+        printk(KERN_ALERT "Failed to create the device\n");
+        return PTR_ERR(ebbcharDevice);
+    }
+    printk(KERN_INFO "GPIO_LKM: device class created correctly\n"); // Made it! device was initialized
+
+   
+   return result;
+}
+
+/** @brief The LKM cleanup function
+ *  Similar to the initialization function, it is static. The __exit macro notifies that if this
+ *  code is used for a built-in driver (not a LKM) that this function is not required. Used to release the
+ *  GPIOs and display cleanup messages.
+ */
+static void __exit ebbgpio_exit(void){
+    device_destroy(ebbcharClass, MKDEV(majorNumber, 0));     // remove the device
+    class_unregister(ebbcharClass);                          // unregister the device class
+    class_destroy(ebbcharClass);                             // remove the device class
+    unregister_chrdev(majorNumber, DEVICE_NAME);             // unregister the major number
+
+   printk(KERN_INFO "GPIO_TEST: The button state is currently: %d\n", gpio_get_value(gpioButton));
+   free_irq(irqNumber, NULL);               // Free the IRQ number, no *dev_id required in this case
+   gpio_unexport(gpioButton);               // Unexport the Button GPIO
+   gpio_free(gpioButton);                   // Free the Button GPIO
+   printk(KERN_INFO "GPIO_TEST: Goodbye from the LKM!\n");
+}
+
+
+/// This next calls are  mandatory -- they identify the initialization function
+/// and the cleanup function (as above).
+module_init(ebbgpio_init);
+module_exit(ebbgpio_exit);
