6d7c41fa14ce52b6426d1660467a6df93a508293
Yoseph Maguire <yoseph.maguire@gmail.om>
2017-12-14 09:46:15 -0600

    fixed ordering mechanism

--- bbb_iot/order_egg.sh
+++ bbb_iot/order_egg.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-aws dynamodb update-item --table-name BuyList --key '{"Product": {"S": "Tea"}}' \
-    --update-expression "ADD Quantity :val" \
-    --expression-attribute-values '{":val":{"N": "1"}}' \
-    --return-values ALL_NEW
+
+itemjson="{\"Product\": {\"S\": \"Egg\"},\"Timestamp\": {\"S\": \"$(date)\"},\"Quantity\": {\"N\": \"12\"}}"
+
+echo $itemjson > eggitem.json
+
+
+aws dynamodb put-item --table-name BuyList \
+    --item file://eggitem.json 
 
 
